179cce1c38159c08c3767236357746cee662beb5
Todd Schwebach <todd.schwebach1@t-mobile.com>
2020-09-17 16:28:42 -0700

    Checkin base project

---
+++ src/test/java/com/slalom/sluber/SluberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slalom.sluber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SluberServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
