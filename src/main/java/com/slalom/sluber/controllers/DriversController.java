4849913993ab7aac65ae2d6b1ffbb6a3f7fdd4a8
Todd Schwebach <todd.schwebach@slalom.com>
2020-09-24 09:49:14 -0700

    Only generate API interface

--- src/main/java/com/slalom/sluber/controllers/DriversController.java
+++ src/main/java/com/slalom/sluber/controllers/DriversController.java
@@ -4,8 +4,11 @@ import com.slalom.sluber.api.SluberApi;
 import com.slalom.sluber.api.models.DriverDetails;
 import com.slalom.sluber.api.models.DriverResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class DriversController implements SluberApi {
+
     @Override
     public ResponseEntity<DriverResponse> createDriver(DriverDetails body) {
         DriverResponse driverResponse = new DriverResponse();
